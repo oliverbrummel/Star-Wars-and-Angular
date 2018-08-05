dbcd0324bcc7f15103b594fbafe09f630c3c657b
Oliver Brummel <oliverbrummel1@gmail.com>
2018-08-05 18:13:43 -0500

    Add dialog and button components

--- src/app/custom-modules/my-angular-material/my-angular-material.module.ts
+++ src/app/custom-modules/my-angular-material/my-angular-material.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatGridListModule, MatCardModule, MatIconModule } from '@angular/material';
+import {
+  MatGridListModule,
+  MatCardModule,
+  MatIconModule,
+  MatDialogModule,
+  MatButtonModule
+} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     MatGridListModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatDialogModule,
+    MatButtonModule
   ],
   exports: [
     MatGridListModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatDialogModule,
+    MatButtonModule
   ],
   declarations: []
 })
-export class MyAngularMaterialModule { }
+export class MyAngularMaterialModule {}
