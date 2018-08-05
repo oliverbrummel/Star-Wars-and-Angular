95f9f90a9ad2c1e415b271c0e07fea00f309cee2
Oliver Brummel <oliverbrummel1@gmail.com>
2018-08-05 04:54:00 -0500

    test out the regex

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -14,6 +14,16 @@ export class MainContentComponent implements OnInit {
 
   constructor(private starwarsService: StarwarsService) { }
 
+  testClick(character: object): any {
+    console.log('character', character);
+    this.findIdFromUrl(character['url']);
+  }
+
+  findIdFromUrl(url: string): any {
+    const id: any = url.match(/([0-9])+/g);
+    return id[0];
+  }
+
   ngOnInit() {
     this.starwarsService.getCharacters('https://swapi.co/api/people/?page=1')
     .subscribe(response => this.listItems = response);
